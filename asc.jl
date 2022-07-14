183605468be269578835562a469e51c64ea98a73
Taimoor Sohail <taimoorsohail@gmail.com>
2022-07-14 21:17:46 +1000

    Changed  function name to

--- asc.jl
+++ asc.jl
@@ -20,7 +20,7 @@ Nx, Ny, Nz = 128, 128, 64
 decay = Nz/Lz*2
 
 
-stretched_grid(k) = Lz * (tanh(decay * (2Nz + 3/2)) - tanh(decay * (Nz + 1/2 + k))) / 
+z_faces(k) = Lz * (tanh(decay * (2Nz + 3/2)) - tanh(decay * (Nz + 1/2 + k))) / 
                             (tanh(decay * (Nz + 3/2)) - tanh(decay * (2Nz + 3/2)))
 
 
